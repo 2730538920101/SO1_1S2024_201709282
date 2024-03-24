399582fc009ab4cfe96839a174e2473a2e20c1da
2730538920101 <2730538920101@ingenieria.usac.edu.gt>
2024-03-24 09:02:45 -0600

    [UPDATE] Tarea 3 v1.1

--- tareas/[SO1]T3_201709282/publisher/index.js
+++ tareas/[SO1]T3_201709282/publisher/index.js
@@ -4,14 +4,24 @@ const redis = require('redis');
 // Conectarse a la instancia de Redis en Memory Store
 const client = redis.createClient({
    host: process.env.REDIS_HOST,
-   port: process.env.REDIS_PORT
+   port: process.env.REDIS_PORT,
+   password: process.env.REDIS_PASSWORD
 });
 
-// Publicar un mensaje en el canal 'test'
+let counter = 0;
+const maxMessages = 10; // Cambia esto según tus necesidades
+
+// Publicar un mensaje en el canal 'test' y cerrar el cliente después de un número determinado de repeticiones
 function publishMessage() {
    const message = JSON.stringify({ msg: "Hola a todos" });
-   client.publish('test', message);
+   client.publish('test', message, () => {
+      counter++;
+      if (counter >= maxMessages) {
+         clearInterval(intervalId);
+         client.quit(); // Cerrar el cliente después de completar todas las publicaciones
+      }
+   });
 }
 
 // Publicar un mensaje cada segundo (solo para demostración)
-setInterval(publishMessage, 1000);
+const intervalId = setInterval(publishMessage, 1000);
